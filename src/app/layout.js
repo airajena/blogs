6717452566a7b98cbd76b176e77b020566b2b507
Aira Jena <airajena.hack@gmail.com>
2025-02-24 11:08:15 +0530

    style: Add fonts and setup tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { cx } from "../utils";
+const inter = Inter({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-in",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mr",
 });
 
 export const metadata = {
@@ -19,8 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       <body
+        className={cx(
+          inter.variable,
+          manrope.variable,
+          "font-mr bg-light dark:bg-dark"
+        )}
       >
         {children}
       </body>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,17 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#fff",
+        accent: "#7B00D3",
+        accentDark: "#ffdb4d",
+        gray: "#747474",
+      },
+      fontFamily:{
+        mr: ["var(--font-mr)"],
+        in: ["var(--font-in)"]
+      },
     },
   },
   plugins: [],
